4915f73a1a6ad72a2980bd6b9ece4edc3995b432
Ian Santopietro <isantop@system76.com>
2019-05-01 14:06:06 -0600

    Fix Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -24,7 +26,7 @@ copyright = '2019, Ian Santopietro'
 author = 'Ian Santopietro'
 
 # The short X.Y version
-version = ''
+version = '0.0.8'
 # The full version, including alpha/beta/rc tags
 release = ''
 
@@ -74,7 +76,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
