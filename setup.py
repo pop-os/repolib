02ab97eac05e37563b6888b4efebb9aa7493d65e
Ian Santopietro <ian@system76.com>
2020-08-20 17:11:42 -0600

    build(python): Updates for version tracking
    Now includes version tracking built-in with utilities for testing/releasing built into setup.py

--- setup.py
+++ setup.py
@@ -28,12 +28,17 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import subprocess
 
-from setuptools import setup
+from distutils.core import setup
 from distutils.cmd import Command
+import os
+import subprocess
+import sys
 
-version = {}
-with open("repolib/__version__.py") as fp:
-    exec(fp.read(), version)
+def get_version():
+    version = {}
+    with open(os.path.join('repolib', '__version__.py')) as fp:
+        exec(fp.read(), version)
+    return version['__version__']
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
@@ -52,33 +57,125 @@ classifiers = [
     'Topic :: System'
 ]
 
-def do_deb_release(vers):
-    subprocess.run(['dch', '-v', vers])
-    subprocess.run(['dch', '-r','""'])
-
-def run_pytest():
-    subprocess.run(['pytest-3'])
-
-class DebRelease(Command):
-    """Update Debian Changelog with latest version in code."""
-    description = 'Release a version to the debian packaging.'
+class Release(Command):
+    """ Generate a release and push it to git."""
+    description = "Generate a release and push it to git."
 
     user_options = [
-        ('version', None, 'Override the version to use'),
+        ('dry-run', None, 'Skip the actual release and do a dry run instead.'),
+        ('skip-deb', None, 'Skip doing a debian update for this release.'),
+        ('skip-git', None, 'Skip committing to git at the end.'),
+        ('prerelease=', None, 'Release a pre-release version (alpha,beta,rc)'),
+        ('increment=', None, 'Manually specify the desired increment (MAJOR, MINOR, PATCH)')
     ]
 
     def initialize_options(self):
-        self.version = version['VERSION']
-
+        self.dry_run = False
+        self.skip_deb = False
+        self.skip_git = False
+        self.prerelease = None
+        self.increment = None
+    
     def finalize_options(self):
         pass
 
     def run(self):
-        do_deb_release(self.version)
+        cz_command = ['cz', 'bump', '--yes']
+        ch_command = ['dch']
+        git_command = ['git', 'add', '.']
 
+        def capture_version(sp_complete):
+            output = sp_complete.stdout.decode('UTF-8').split('\n')
+            print('\n'.join(output))
+            for line in output:
+                    if 'tag to create' in line:
+                            version_line = line
+            
+            try:
+                return version_line.split()[-1].replace('v', '')
+            except UnboundLocalError:
+                stderr = sp_complete.stderr.decode('UTF-8')
+                print("WARNING: Couldn't get updated version! Using current.")
+                print(stderr)
+                return get_version()
+
+        if self.dry_run:
+            print('Dry run: Not making actual changes')
+            cz_command.append('--dry-run')
+        
+        if self.prerelease:
+            if self.prerelease.lower() not in ['alpha', 'beta', 'rc']:
+                raise Exception(
+                    f'{self.prerelease} is not a valid prerelease type. Please '
+                    'use one of "alpha", "beta", or "rc".'
+                )
+            cz_command.append('--prerelease')
+            cz_command.append(self.prerelease.lower())
+        
+        if self.increment:
+            if self.increment.upper() not in ['MAJOR', 'MINOR', 'PATCH']:
+                raise Exception(
+                    f'{self.increment} is not a valid increments. Please use '
+                    'one of MAJOR, MINOR, or PATCH.'
+                )
+            cz_command.append('--increment')
+            cz_command.append(self.increment.upper())
+        
+        # We need to get the new version from CZ, as the file hasn't been 
+        # updated yet.
+        version_command = cz_command.copy()
+        version_command.append('--dry-run')
+        version_complete = subprocess.run(version_command, capture_output=True)
+        
+        version = capture_version(version_complete)
+        print(f'Old Version: {get_version()}')
+        print(f'New version: {version}')
+
+        ch_command.append(f'-v{version}')
+        if not self.skip_deb:
+            print(ch_command)
+            if not self.dry_run:
+                subprocess.run(ch_command)
+                subprocess.run(['dch', '-r', '""'])
+        
+        if not self.skip_git:
+            print(git_command)
+            if not self.dry_run:
+                subprocess.run(git_command)
+
+        print(' '.join(cz_command))
+        if not self.dry_run:
+            subprocess.run(cz_command)
+
+class Test(Command):
+    """ Run pyflakes and pytest"""
+    description = 'Run pyflakes and pytest'
+
+    user_options = [
+        ('skip-flakes', None, 'Skip running pyflakes'),
+        ('skip-test', None, 'Skip running pytest')
+    ]
+
+    def initialize_options(self):
+        self.skip_flakes = False
+        self.skip_test = False
+    
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        pytest_command = ['pytest-3']
+        flakes_command = ['pyflakes3', 'repolib']
+        
+        if not self.skip_test:
+            subprocess.run(pytest_command)
+
+        if not self.skip_flakes:
+            subprocess.run(flakes_command)
+            
 setup(
     name = 'repolib',
-    version = version['VERSION'],
+    version = get_version(),
     author = 'Ian Santopietro',
     author_email = 'isantop@gmail.com',
     url = 'https://github.com/isantop/repolib',
@@ -88,7 +185,7 @@ setup(
     tests_require = ['pytest'],
     license = 'BSD-2',
     packages=['repolib'],
-    cmdclass={'release': DebRelease},
+    cmdclass={'release': Release, 'test': Test},
     scripts=['bin/apt-manage'],
     data_files=[
         ('share/bash-completion/completions', ['data/apt-manage']),
