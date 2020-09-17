d003feb478a60d67c494d92077e4fa838a1e2736
Ian Santopietro <ian@system76.com>
2020-09-17 11:35:40 -0600

    fix: skip pylint on service file for now

--- data/service.py
+++ data/service.py
@@ -17,6 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with Repolib.  If not, see <http://www.gnu.org/licenses/>.
 '''
+#pylint: skip-file
 
 import gi
 from gi.repository import GObject, GLib
