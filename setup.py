77dd693b5e7d2fc5a6a04407779af6ef3f842e8d
Ian Santopietro <ian@system76.com>
2020-11-09 10:47:06 -0700

    build(python): Skip pylint in setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with RepoLib.  If not, see <https://www.gnu.org/licenses/>.
 """
-#pylint: disable=invalid-name,subprocess-run-check
+#pylint: skip-file
 # We don't need to check these in setup
 
 import os
